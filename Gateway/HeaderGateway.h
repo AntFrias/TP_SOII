90cfb008891f4cc8b97b505bf463dc195aaeb11d
Unknown <a21230577@isec.pt>
2018-05-02 15:00:03 +0100

    inicio do gw

--- Gateway/HeaderGateway.h
+++ Gateway/HeaderGateway.h
@@ -12,17 +12,46 @@
 #include <wchar.h>
 #include <shellapi.h>
 
-
+//Define
+#define Buffer_size 32
+TCHAR nomeSrtoGW[] = TEXT("Buffer SrToGw");
+TCHAR nomeGwtoSr[] = TEXT("Buffer GwToSr");
 
 //prototipos
 
 void IniciarGateway();
 
-typedef struct MemoriaPartilhada {
+//Estruturas
 
-	HANDLE ptrGWtoServer;
-	HANDLE ptrServertoGW;
-	HANDLE ptrMapJogo;
+typedef struct Packet {
+
+	int tipo;
+	int session_id;
+	int pontuacao;
+
+	union datPacket
+	{
+		TCHAR nome[10];
+		int movimento;
+		int tiro;
+		//PowerUpp PowerUp;
+	}dataPacket;
+
+}packet;
+
+typedef struct BufferMsg {
+
+	packet array[Buffer_size];
+	unsigned int in, out;
+
+}bufferMsg, *ptrbufferMsg;
+
+typedef struct Gestao_gateway {
 
 
-}M;
+	int sessionID;
+	bufferMsg *comSertoGw;
+	bufferMsg *comGwtoSer;
+
+
+}dataGw;
