40e7bfdadc0bdbaa92eb4dd4057119f39301163d
Unknown <a21230573@isec.pt>
2018-05-07 18:16:55 +0100

    Local - Teste DLL a funcionar

--- Gateway/Gateway.cpp
+++ Gateway/Gateway.cpp
@@ -1,115 +1,29 @@
 #include "HeaderGateway.h"
 #include "../AcessoMemDLL/stdafx.h"
 #pragma comment(lib, "../x64/Debug/AcessoMemDLL.lib")
-//int * global_int;
-//BufferMsg * auxbuff;
+
 
 dataGw dadosGw;
-synBuffer sync;
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-int CriaSyncMemoria() {
 
-	_tprintf(TEXT("\nVAI CRIAR O SEMAPHORE :  %s"), semGwLer);
+void EncaminhamentoPacotesGwtoServ() {
 
-	sync.SemGwtoServComItem = CreateSemaphore(NULL, 0, Buffer_size, semGwLer);
-	if (sync.SemGwtoServComItem == NULL) {
-		_tprintf(TEXT("Erro ao criar Semaforo %s"), semGwLer);
-		return -1;
-	}
+	Packet Pacote;
 
-
-	_tprintf(TEXT("\nVAI CRIAR O SEMAPHORE :  %s"), semGwEscrever);
-
-	sync.SemGwtoServSemItem = CreateSemaphore(NULL, Buffer_size, Buffer_size, semGwEscrever);
-	if (sync.SemGwtoServSemItem == NULL) {
-		_tprintf(TEXT("Erro ao criar Semaforo %s"), semGwEscrever);
-		return -1;
-	}
-
-	
-	return 0;
-}///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// cria Buffer na memoria partilhada ////////////////////////////////////////////////////////////////////////////////////
-void criaMemoriaPartilhada(ptrbufferMsg *aux, LPCTSTR nomeBuffer) {
-
-	HANDLE hBuffer = NULL;
-	//criar a memoria partilhada 
-	hBuffer = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, sizeof(bufferMsg), nomeBuffer);
-
-	if (hBuffer == NULL) {
-		_tprintf(TEXT("\nErro a criar Buffer %s na memoria Partilhada "), nomeBuffer);
-	}
-	else if (GetLastError() == ERROR_ALREADY_EXISTS) {
-		_tprintf(TEXT("\nEste buffer -> %s ja foi criado\n"), nomeBuffer);
-
-	}
-	// mapear a memoria para a mensagem
-	*aux = (ptrbufferMsg)MapViewOfFile(hBuffer, FILE_MAP_WRITE, 0, 0, sizeof(bufferMsg));
-
-	if (*aux == NULL) {
-
-		_tprintf(TEXT("Erro a mapear Buffer de mensagem"));
-	}
-
-	(*aux)->tail = 0;
-
-	(*aux)->head = Buffer_size;
-
-}/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-void escreveNoBuffer(Packet *pacoteParaEscrita) {
-	
-	int head = dadosGw.comGwtoSer->head;
-	int tail = dadosGw.comGwtoSer->tail;
-
-	EnterCriticalSection(&sync.MutexGwtoSer);
-	
-				dadosGw.comGwtoSer->array[tail] = *pacoteParaEscrita;
-			
-				wcscpy_s(dadosGw.comGwtoSer->array[tail].dataPacket.nome,pacoteParaEscrita->dataPacket.nome);
-			 
-		
-			
-
-				if (dadosGw.comGwtoSer->head == Buffer_size)
-					dadosGw.comGwtoSer->head = dadosGw.comGwtoSer->tail;
-
-				dadosGw.comGwtoSer->tail = ++dadosGw.comGwtoSer->tail % Buffer_size;
-			
-	LeaveCriticalSection(&sync.MutexGwtoSer);
-	
+	escrevebufferGwToSr(&Pacote);
 }
 
-void escrevebufferGwToSr() {
-	
-	Packet auxPacote;
-	
-
-	do {
-		
-		auxPacote.tipo = 1;
-
-		_fgetts(auxPacote.dataPacket.nome, 10,stdin);
-
-		WaitForSingleObject(sync.SemGwtoServSemItem, INFINITE);
-		
-				escreveNoBuffer(&auxPacote);
-				
-		ReleaseSemaphore(sync.SemGwtoServComItem, 1, NULL);
-		
-	} while (1);
-	
-}
-
+// funçao que inicia os serviços do Gateway
 void IniciarGateway() {
 
-	criaMemoriaPartilhada(&dadosGw.comGwtoSer, nomeGwtoSr);
+	Packet Pacote;
+
+	_tprintf(TEXT("\n\n Inicializaçao do Gateway\n\n"));
+
+	CriaMemoriaPartilhada();
 
 	CriaSyncMemoria();
 
-	InitializeCriticalSection(&sync.MutexGwtoSer);
-
-	dadosGw.hThreadGwtoSer = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)escrevebufferGwToSr, (LPVOID)NULL, 0, &dadosGw.idThreadGwtoSer);
+	dadosGw.hThreadGwtoSer = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)EncaminhamentoPacotesGwtoServ, (LPVOID)NULL, 0, &dadosGw.idThreadGwtoSer);
 
 }
 
@@ -119,17 +33,8 @@ int _tmain(int argc, LPTSTR argv[]) {
 	_setmode(_fileno(stdin), _O_WTEXT);
 	_setmode(_fileno(stdout), _O_WTEXT);
 #endif	
-
-	_tprintf(TEXT("side of packet %d "), sizeof(Packet));
-	
-
-	_tprintf(TEXT("\nconta: %d \n shared int created \n"),sum(1,1));
 	
 	IniciarGateway();
-
-
-
-	_tprintf(TEXT("\n\n Inicializaçao do Gateway\n\n"));
 
 	Sleep(190000);
 	return 0;
--- Gateway/HeaderGateway.h
+++ Gateway/HeaderGateway.h
@@ -12,20 +12,6 @@
 #include <wchar.h>
 #include <shellapi.h>
 
-//Define
-#define Buffer_size 32 
-TCHAR nomeGwtoSr[] = TEXT("Buffer GwToSr");
-TCHAR nomeSrtoGW[] = TEXT("Buffer SrToGw");
-
-
-TCHAR semGwLer[] = TEXT("semaforoGwLer");
-TCHAR semGwEscrever[] = TEXT("semaforoGwEscrever");
-TCHAR mutexGwtoSer[] = TEXT("mutexGwtoSer");
-
-TCHAR semSerLer[] = TEXT("semaforoSerLer");
-TCHAR semSerEscrever[] = TEXT("semaforoSerEscrever");
-TCHAR mutexSertoGw[] = TEXT("mutexSertoGw");
-////////////////////////////////////////////
 
 //prototipos
 
@@ -33,43 +19,7 @@ void IniciarGateway();
 
 //Estruturas
 
-typedef struct synbuff {
 
-	HANDLE SemGwtoServComItem;
-	HANDLE SemGwtoServSemItem;
-	CRITICAL_SECTION MutexGwtoSer;
-
-	HANDLE SemSerLer;
-	HANDLE SemSerEscrever;
-	CRITICAL_SECTION MutexSertoGw;
-
-}synBuffer;
-
-
-typedef struct Packet {
-
-	int tipo;
-	int session_id;
-	int pontuacao;
-
-	union datPacket
-	{
-		TCHAR nome[10];
-		int movimento;
-		int tiro;
-		//PowerUpp PowerUp;
-	}dataPacket;
-
-}packet;
-
-typedef struct BufferMsg {
-
-	packet array[Buffer_size];
-	//packet array;
-	int tail;
-	int head;
-
-}bufferMsg, *ptrbufferMsg;
 
 typedef struct Gestao_gateway {
 
@@ -77,7 +27,5 @@ typedef struct Gestao_gateway {
 	int sessionID;
 	HANDLE hThreadGwtoSer;
 	DWORD idThreadGwtoSer;
-	bufferMsg *comSertoGw;
-	bufferMsg *comGwtoSer; 
 
 }dataGw;
