91986dc5f5d2114ae910a7790edc9e49635e4c5b
Unknown <a21230577@isec.pt>
2018-06-06 00:31:18 +0100

    cliente + organizado
    aprovação do frias

--- Cliente/Cliente.cpp
+++ Cliente/Cliente.cpp
@@ -23,14 +23,14 @@ void Envia(LPVOID NUUL) {
 		_fgetts(Cliente.nome, 10, stdin);
 		PacoteEnvio.tipo = 1;
 		wcscpy_s(PacoteEnvio.dataPacket.nome, Cliente.nome);
-		
+
 		//inicializa coisas para o named pipe
 		ZeroMemory(&Ov, sizeof(Ov));
 		ResetEvent(IOReady);
 		Ov.hEvent = IOReady;
-		
+
 		//escrever no pipe
-		WriteFile(Cliente.pipe, &PacoteEnvio,sizeof(packet), &nBytesLidos, &Ov);
+		WriteFile(Cliente.pipe, &PacoteEnvio, sizeof(packet), &nBytesLidos, &Ov);
 		WaitForSingleObject(IOReady, INFINITE);
 		ret = GetOverlappedResult(Cliente.pipe, &Ov, &nBytesLidos, FALSE);
 
@@ -39,23 +39,16 @@ void Envia(LPVOID NUUL) {
 			break;
 		}
 		_tprintf(TEXT("[CLIENTE] Enviei %d bytes ao GATEWAY ...(WriteFile)\n"), nBytesLidos);
-		
 
-		
+
+
 	} while (Cliente.alive);
 
 
 }
 
+void escuta() {
 
-int _tmain(int argc, LPTSTR argv[]) {
-
-
-#ifdef UNICODE  //UNICODE
-	_setmode(_fileno(stdin), _O_WTEXT);
-	_setmode(_fileno(stdout), _O_WTEXT);
-#endif	
-	
 	packet PacoteRecebido;
 	DWORD  nBytesLidos;
 	BOOL ret;
@@ -68,20 +61,20 @@ int _tmain(int argc, LPTSTR argv[]) {
 		_tprintf(TEXT("Erro ao criar evento\n"));
 		exit(-1);
 	}
-									
+
 
 	//ler do pipe
 	if (!WaitNamedPipe(PIPE_NAME, NMPWAIT_WAIT_FOREVER)) {
-	_tprintf(TEXT(" N�o consegui ligar ao pipe '%s'!\n"), PIPE_NAME);
-	exit(-1);
+		_tprintf(TEXT(" N?o consegui ligar ao pipe '%s'!\n"), PIPE_NAME);
+		exit(-1);
 	}
 
 	Cliente.alive = 1;
 	Cliente.pipe = CreateFile(PIPE_NAME, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0 | FILE_FLAG_OVERLAPPED, NULL);
 
 	if (Cliente.pipe == NULL) {
-	_tprintf(TEXT("N�o consegui ligar ao pipe '%s'!\n"), PIPE_NAME);
-	exit(-1);
+		_tprintf(TEXT("N?o consegui ligar ao pipe '%s'!\n"), PIPE_NAME);
+		exit(-1);
 	}
 
 	// mudar para modo message
@@ -90,10 +83,6 @@ int _tmain(int argc, LPTSTR argv[]) {
 	fres = SetNamedPipeHandleState(Cliente.pipe, &dwMode, NULL, NULL);
 	if (!fres)
 		_tprintf(TEXT("ERRO  a  mudar para message mode"));
-
-	//escrevo no pipe
-	Cliente.ht = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Envia, (LPVOID)NULL, 0, &Cliente.IDth);
-
 
 	while (1) {
 
@@ -105,19 +94,52 @@ int _tmain(int argc, LPTSTR argv[]) {
 
 		WaitForSingleObject(IOReady, INFINITE); // espera pelo evento
 
-		ret = GetOverlappedResult(Cliente.pipe, &Ov, &nBytesLidos, FALSE); // se mal d� 0 !!!
+		ret = GetOverlappedResult(Cliente.pipe, &Ov, &nBytesLidos, FALSE); // se mal d? 0 !!!
 
 		if (!ret || !nBytesLidos) {
 			_tprintf(TEXT("Nao li nada\n"), ret, nBytesLidos);
 			break;
 		}
-		_tprintf(TEXT("\n\nRecebi este nome %s\n"),PacoteRecebido.dataPacket.nome);
+		_tprintf(TEXT("\n\nRecebi este nome %s\n"), PacoteRecebido.dataPacket.nome);
 	}
 
 	CloseHandle(Cliente.pipe);	// fecha pipe do cliente
 	CloseHandle(IOReady);		// fecha handle dop cliente 
 
-	WaitForSingleObject(Cliente.ht, INFINITE); //espera pela thread de cima
+
+
+
+}
+
+
+
+
+void IniciaCliente() {
+
+
+	//thread escrevo no pipe
+	Cliente.ht = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Envia, (LPVOID)NULL, 0, &Cliente.IDth);
+
+	//le do pipe
+	escuta();
+
+	//espera pela thread que escreve
+	WaitForSingleObject(Cliente.ht, INFINITE);
+
+
+
+
+}
+
+int _tmain(int argc, LPTSTR argv[]) {
+
+
+#ifdef UNICODE  //UNICODE
+	_setmode(_fileno(stdin), _O_WTEXT);
+	_setmode(_fileno(stdout), _O_WTEXT);
+#endif	
+
+	IniciaCliente();
 
 
 	return 0;
